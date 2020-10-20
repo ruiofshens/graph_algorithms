5dc0ec755648db297d777dcc34b2d57f06f82a57
Shen Rui <chongshenrui98@gmail.com>
2020-10-20 19:23:17 +0800

    First commit

---
+++ src/GraphAlgorithmConsole.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
+public class GraphAlgorithmConsole {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to our application!");
+        System.out.println("Place files to be read in data folder");
+
+    }
+}
