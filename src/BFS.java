18f920dca7e5785d85b750735b5b19f6f3ca5c12
joshlim288 <joshlim0047@e.ntu.edu.sg>
2020-11-01 23:19:01 +0800

    Edited some comments

--- src/BFS.java
+++ src/BFS.java
@@ -13,14 +13,13 @@ public class BFS {
      * For each hospital, we increase the radius of our search by 1 sequentially, searching the same radius for
      * all the hospitals before moving on to radius+1
      *
+     * Does not return any value, but rather outputs the results to a text file called "BFSresults.txt"
+     *
      * In layman's terms, we are connecting all the hospitals to a single source node, and starting our BFS on that
      * @param hospitals int[] of hospital nodes in the graph, stored as their node id
      * @param adj Graph to be searched, in the form of an adjacency list implemented as a HashMap, with node id as
      *            first argument. This is to accommodate non-contiguous node ids
      * @param maxNodeId The maximum node id that the graph contains
-     * @return The paths from each node to the nearest hospital, in the form of an array of LinkedLists. The LinkedList
-     * contains a value of -1 if the node id is not present in the graph, and is an empty LinkedList if the node is
-     * present in the graph, but is not connected to a hospital
      */
     public static void search(int[] hospitals, HashMap<Integer, ArrayList<Integer>> adj, int maxNodeId)
     {
@@ -51,15 +50,15 @@ public class BFS {
         int currentNode;
         ArrayList<Integer> currentPath;
         while (queue.size() != 0) {
-            // Dequeue a vertex from queue and print it
+            // Dequeue a vertex from queue and store it's current path
             currentNode = queue.poll();
             currentPath = paths[currentNode];
             // Get all adjacent nodes of currentNode
             for (int n : adj.get(currentNode)) {
-                // If an adjacent node has not been visited, then mark it
                 if (!visited[n]) {
+                    // If an adjacent node has not been visited, then mark it
                     visited[n] = true;
-                    // Enqueue the
+                    // Enqueue the adjacent node for examination later
                     queue.add(n);
                     // Add the adjacent node as the first node in its own path
                     paths[n].add(n);
@@ -73,6 +72,11 @@ public class BFS {
         outputResults(paths, maxNodeId);
     }
 
+    /**
+     * Outputs results of the search to a text file
+     * @param paths Result from the BFS search, contains the paths of each node to their nearest hospital
+     * @param maxNodeId Max nodeId, used to generate the output list
+     */
     private static void outputResults(ArrayList<Integer>[] paths, int maxNodeId) {
         try {
             System.out.print("Enter name of file to save output to (e.g. BFSresults.txt): ");
